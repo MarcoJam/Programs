696b7510b53b31bf304e75fcd5c5b7e8b3e35bc5
MarcoJam <marcocecchimail@gmail.com>
2015-05-15 18:52:35 +0200

    Forza Quattro
    Creazione Progetto 15/05/15

---
+++ src/gj/forza4/player/Cecchi.java
@@ -0,0 +1,23 @@
+package gj.forza4.player;
+
+public class Cecchi implements Player {
+
+	@Override
+	public int move() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void start(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void tellMove(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
